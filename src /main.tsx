5a2d7b6bdf34a43e6267210e9ff9c15d1040d7c3
todaymusic <todaymusic2407@gmail.com>
2026-04-21 14:59:27 +0900

    Update main.tsx

--- src /main.tsx
+++ src /main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
 import "./styles.css";
+
+const router = getRouter();
 
 const rootElement = document.getElementById("root")!;
 createRoot(rootElement).render(
