ea5a8e8fa888f2668c77485facb2f525ab59443b
todaymusic <todaymusic2407@gmail.com>
2026-04-22 10:27:40 +0900

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -57,6 +57,7 @@ function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => vo
 export const getRouter = () => {
   const router = createRouter({
     routeTree,
+    basepath: "/english-lesson-planner",
     context: {},
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
